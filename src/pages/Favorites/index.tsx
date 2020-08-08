06d57516df5b9f6798f5099c218ef28eea125585
sambuite <murilosambuite@gmail.com>
2020-08-08 20:17:33 -0300

    feat: add a scroll view with a list of TeacherItem components

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import { Container } from './styles';
+import { Container, StyledScrollView } from './styles';
 
 const Favorites = () => {
   return (
     <Container>
       <PageHeader title="Meus proffys favoritos" />
+      <StyledScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 8,
+          paddingBottom: 14,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </StyledScrollView>
     </Container>
   );
 };
--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   background-color: #f0f0f7;
+`;
+
+export const StyledScrollView = styled(ScrollView)`
+  margin-top: -40px;
+  padding: 16px;
 `;
